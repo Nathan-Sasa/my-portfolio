b9f58b319efb710834615c2b0860f9c865528d49
Nathan-Sasa <jonathanbikuta05@gmail.com>
2026-02-23 03:07:39 +0100

    install: lucide-icon

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,19 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
-        
+        path: 'landing',
+        title: 'NathanDev - Portfolio',
+        loadComponent: () => import('./feature/landing/landing.component').then(m => m.LandingComponent)
+    },
+
+    {
+        path: '',
+        redirectTo: '/landing',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        title: '404 - Not Found',
+        loadComponent: () => import('./shared/layout/not-found/not-found.component').then(m => m.NotFoundComponent)
     }
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -2,15 +2,24 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ThemeComponent } from './shared/layout/theme/theme.component';
 
+import { LucideAngularModule, FileIcon, SearchCode } from 'lucide-angular';
+
 @Component({
 	selector: 'app-root',
 	imports: [
 		RouterOutlet,
-		ThemeComponent
+		ThemeComponent,
+		LucideAngularModule
 	],
 	templateUrl: './app.html',
 	styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('my-portfolio');
+//   readonly fileIcon = FileIcon;
+
+  readonly icons = {
+	searchCode: SearchCode,
+	fileIcon: FileIcon
+  }
 }
