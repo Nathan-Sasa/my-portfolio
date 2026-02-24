e54af8d7a48c9f034ca6bf616da0d336cc2e5a6d
Nathan-Sasa <jonathanbikuta05@gmail.com>
2026-02-24 19:49:25 +0100

    feat: hero animation. add: skill.component

--- src/app/feature/landing/landing.component.ts
+++ src/app/feature/landing/landing.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/layout/header/header.component';
 import { HeroComponent } from '../../shared/components/hero/hero.component';
 import { WorkComponent } from '../../shared/components/work/work.component';
+import { SkillComponent } from '../../shared/components/skill/skill.component';
 
 @Component({
 	selector: 'app-landing',
 	imports: [
 		HeaderComponent,
 		HeroComponent,
-		WorkComponent
+		WorkComponent,
+		SkillComponent
 	],
 	templateUrl: './landing.component.html',
 	styleUrl: './landing.component.css',
