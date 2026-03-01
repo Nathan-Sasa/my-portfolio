6ed2cd59f2a1ee96f4676d6be6117d710b4cfc6a
Nathan-Sasa <jonathanbikuta05@gmail.com>
2026-03-01 03:14:41 +0100

    feat: meComponent photo

--- src/app/shared/components/me/me.component.ts
+++ src/app/shared/components/me/me.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from'@angular/common'
 import { RouterModule } from '@angular/router';
+import { ScrollAnimDirective } from '../../directive/scroll-anim.directive';
 
 @Component({
 	selector: 'app-me',
 	imports: [
 		CommonModule,
-		RouterModule
+		RouterModule,
+		ScrollAnimDirective
 	],
 	templateUrl: './me.component.html',
 	styleUrl: './me.component.css',
 })
 export class MeComponent {
 
+	me = {
+		name: 'Nathan BIKUTA SASA',
+		photo: 'assets/img/images/profile/nathan-profil.webp',
+	}
 }
